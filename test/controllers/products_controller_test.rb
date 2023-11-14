b021d7022defdaf39ef93a05bdb861562cacc69d
debug <1027714618@qq.com>
2023-11-14 15:31:40 +0000

    Rails validates

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,9 +3,17 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title: "Rails test",
+      description: "Rails test 123456",
+      image_url: "rails.png",
+      price: 19.95
+    }
   end
 
   test "should get index" do
+    
+    puts products_url
     get products_url
     assert_response :success
   end
@@ -17,7 +25,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product:  @update  }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +42,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product:  @update  }
     assert_redirected_to product_url(@product)
   end
 
